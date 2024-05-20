479e18555c8626781bdd299dd3e4689d7f373c07
AnastasiiaKochurenkovaPM <anastasiia.kochurenkova.pm.2020@lpnu.ua>
2024-05-20 17:14:56 +0300

    labir

--- game.py
+++ game.py
@@ -29,7 +29,7 @@ class Player(GameSprite):
         keys = key.get_pressed()
         if keys[K_LEFT] and self.rect.x > 5:
             self.rect.x -= self.speed
-        if keys[K_RIGHT] and self.rect.x < 700 - 80:
+        if keys[K_RIGHT] and self.rect.x < 900 - 80:
             self.rect.x += self.speed
         if keys[K_UP] and self.rect.y > 5:
             self.rect.y -= self.speed
@@ -39,26 +39,29 @@ class Player(GameSprite):
 
 # клас-спадкоємець для спрайта-ворога (переміщається сам)
 class Enemy(GameSprite):
-    # direction = "left"
-    # def update(self):
-    #     if self.rect.x <= 470:
-    #         self.direction = "right"
-    #     if self.rect.x >= 700 - 85:
-    #         self.direction = "left"
-    #     if self.direction == "left":
-    #         self.rect.x -= self.speed
-    #     else:
-    #         self.rect.x += self.speed
-    def update(self, player_rect):
-        if self.rect.x < player_rect.x:
+     direction = "left"
+     def update(self):
+         if self.rect.x <= 470:
+            self.direction = "right"
+         if self.rect.x >= 900 - 85:
+            self.direction = "left"
+         if self.direction == "right":
             self.rect.x += self.speed
-        elif self.rect.x > player_rect.x:
+         else:
             self.rect.x -= self.speed
 
-        if self.rect.y < player_rect.y:
-            self.rect.y += self.speed
-        elif self.rect.y > player_rect.y:
-            self.rect.y -= self.speed
+
+
+    # def update(self, player_rect):
+       # if self.rect.x < player_rect.x:
+       #     self.rect.x += self.speed
+        #elif self.rect.x > player_rect.x:
+        #    self.rect.x -= self.speed
+
+      #  if self.rect.y < player_rect.y:
+       #     self.rect.y += self.speed
+    #    elif self.rect.y > player_rect.y:
+     #       self.rect.y -= self.speed
             
             
 # клас для спрайтів-перешкод
@@ -85,9 +88,20 @@ class Wall(sprite.Sprite):
 
 
 # стіни
-w1 = Wall(154, 205, 50, 100, 20, 10, 100)
+w1 = Wall(0, 0, 0, 124, 203, 106, 4)
 #w2 = Wall(154, 205, 50, 100, 480, 350, 10)
 #w3 = Wall(154, 205, 50, 100, 20, 10, 380)
+w2 = Wall(0, 0, 0, 459, 170, 121, 4)
+w3 = Wall(0, 0, 0, 702, 240, 106, 4)
+w4 = Wall(23, 46, 30, 384, 278, 58, 67)
+
+
+w11 = Wall(128, 128, 0, 120, 200, 8, 250)
+w12 = Wall(128, 128, 0, 120, 200, 340, 8)
+w13 = Wall(128, 128, 0, 455, 100, 8, 250)
+w14 = Wall(128, 128, 0, 455, 100, 150, 8)
+w15 = Wall(128, 128, 0, 660, 120, 8, 160)
+w_final = Wall(255, 255, 255, 384, 278, 58, 67)
 
 # написи
 font.init()
@@ -97,9 +111,9 @@ lose = font.render('YOU LOSE!', True, (180, 0, 0))
 
 
 # Персонажі гри:
-player = Player('sprite11.png', 5, 400, 4)
-monster = Enemy('sprite22.png', 700 - 80, 280, 2)
-final = GameSprite('sprite11.png', 700 - 120, 500 - 80, 0)
+player = Player('меджикарп.png', 5, 300, 4)
+monster1 = Enemy('ворог.png', 700 - 80, 280, 2)
+monster2 = Enemy('ворог.png', 200, 300, 0)
 
 
 game = True
@@ -117,33 +131,42 @@ while game:
     if finish != True:
         window.blit(background, (0, 0))
         player.update()
-        # monster.update()
-        monster.update(player.rect)
+        monster1.update()
+        monster2.update()
+
 
 
         player.reset()
-        monster.reset()
-        final.reset()
+        monster1.reset()
+        monster2.reset()
+        
 
 
         w1.draw_wall()
-        #w2.draw_wall()
-        #w3.draw_wall()
+        w2.draw_wall()
+        w3.draw_wall()
+        w4.draw_wall()
+
+        w11.draw_wall()
+        w12.draw_wall()
+        w13.draw_wall()
+        w14.draw_wall()
+        w15.draw_wall()
+        w_final.draw_wall()
 
 
     # Ситуація "Програш"
-    if sprite.collide_rect(player, monster) or sprite.collide_rect(player, w1) :
+    if sprite.collide_rect(player, monster1) or sprite.collide_rect(player, w11) or sprite.collide_rect(player, w12) or sprite.collide_rect(player, w13) or sprite.collide_rect(player, w14) or sprite.collide_rect(player, w15) or sprite.collide_rect(player, monster2):
         finish = True
         window.blit(lose, (200, 200))
 
 
     # Ситуація "Перемога"
-    if sprite.collide_rect(player, final):
+    if sprite.collide_rect(player, w_final):
         finish = True
         window.blit(win, (200, 200))
        
 
     display.update()
     clock.tick(FPS)
-
 
